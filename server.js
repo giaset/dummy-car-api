ad61524c91dbe14ed6b96e4507e31bdca90f7ce3
Gianni Settino <gianni@wherecloud.com>
2013-12-25 16:29:32 -0500

    Can now upload image files and view them

--- server.js
+++ server.js
@@ -6,19 +6,7 @@ function start(route, handle) {
 		var pathname = url.parse(request.url).pathname;
 		if (pathname != "/favicon.ico") {
 			console.log("\nRequest for " + pathname + " received.");
-
-			request.setEncoding("utf8");
-
-			var postData = "";
-			request.addListener("data", function(postDataChunk) {
-				postData += postDataChunk;
-				//console.log("Received POST data chunk '"+ postDataChunk + "'.");
-			});
-
-			request.addListener("end", function() {
-				route(handle, pathname, response, postData);
-			})
-
+			route(handle, pathname, response, request);
 		}
 	}
 
