96cd7446d3400f164291e083fbfa4f5d9a259b15
Gianni Settino <gianni@wherecloud.com>
2013-12-25 15:28:54 -0500

    Can now handle POST data from a text box

--- router.js
+++ router.js
@@ -1,6 +1,6 @@
-function route(handle, pathname, response) {
+function route(handle, pathname, response, postData) {
 	if (typeof handle[pathname] === 'function') {
-		handle[pathname](response);
+		handle[pathname](response, postData);
 	} else {
 		console.log("No request handler found for " + pathname);
 		response.writeHead(404, {"Content-Type": "text/plain"});
--- server.js
+++ server.js
@@ -6,7 +6,19 @@ function start(route, handle) {
 		var pathname = url.parse(request.url).pathname;
 		if (pathname != "/favicon.ico") {
 			console.log("\nRequest for " + pathname + " received.");
-			route(handle, pathname, response);
+
+			request.setEncoding("utf8");
+
+			var postData = "";
+			request.addListener("data", function(postDataChunk) {
+				postData += postDataChunk;
+				//console.log("Received POST data chunk '"+ postDataChunk + "'.");
+			});
+
+			request.addListener("end", function() {
+				route(handle, pathname, response, postData);
+			})
+
 		}
 	}
 
