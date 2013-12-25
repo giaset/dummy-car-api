8a015a061559204c42fcb38cd022d1ec647e2c0b
Gianni Settino <gianni@wherecloud.com>
2013-12-25 00:18:05 -0500

    Done basic request handling

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 var server = require("./server");
+var router = require("./router");
+var requestHandlers = require("./requestHandlers");
 
-server.start();
+//In JavaScript, objects are just collections of name/value pairs
+//In this case, we map strings (endpoint URLs) to functions (requestHandlers)
+var handle = {};
+handle["/"] = requestHandlers.start;
+handle["/start"] = requestHandlers.start;
+handle["/upload"] = requestHandlers.upload;
+
+server.start(router.route, handle);
